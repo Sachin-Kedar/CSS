a5729dd11ecf5df094c44157d241f848e22328fc
Sachin kedar <kedarsachin1020@gmail.com>
2024-09-30 15:29:09 +0530

    Codes

---
+++ script.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += ' ' + operator + ' ';
+}
+
+function appendDot() {
+    if (!display.value.includes('.')) {
+        display.value += '.';
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
